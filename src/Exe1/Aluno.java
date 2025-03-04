00b53fd7bfa7676d389faf0cfcbb43571b7815f9
RafaSK <rafael012chavess@gmail.com>
2025-03-04 15:24:22 -0300

    Exercicios POO

--- src/Exe1/Aluno.java
+++ src/Exe1/Aluno.java
@@ -2,4 +2,32 @@ package Exe1;
 
 public class Aluno {
 
+    public int nAluno, idade;
+    public String nmAluno;
+    public float p1,p2;
+
+    public Aluno() {
+        nmAluno = "Nome vazio";
+    }
+
+    public Aluno(int nAluno, int idade, String nmAluno, float p1, float p2) {
+        this.nAluno = nAluno;
+        this.idade = idade;
+        this.nmAluno = nmAluno;
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public float notaFinal(){
+        return (p1+p2) / 2.0F;
+    }
+
+    public String passou(){
+        return (notaFinal() >= 6.0F) ? "O aluno passou" : "O aluno não passou";
+    }
+
+    public String dadosAluno(){
+        return "Numero do aluno: " + nAluno + "\nNome do aluno: " + nmAluno + "\nIdade: " + idade + "\nNota Final: " + notaFinal();
+    }
+
 }
--- src/Mains/MainAluno.java
+++ src/Mains/MainAluno.java
@@ -3,6 +3,9 @@ import Exe1.Aluno;
 
 public class MainAluno {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Aluno obj1 = new Aluno(25251,18,"Rafael Chaves Souza", 5.0F, 7.0F);
+        System.out.println(obj1.dadosAluno());
+        System.out.println("\n" + obj1.passou());
     }
 }
